0e833bdc13142df848378d5317b068d623733f91
김보석 <kimboseok@gimboseog-ui-MacBookPro.local>
2021-11-23 16:36:48 +0900

    first commit

---
+++ SwiftUISection2/SwiftUISection2/SwiftUISection2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISection2App.swift
+//  SwiftUISection2
+//
+//  Created by 김보석 on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISection2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
