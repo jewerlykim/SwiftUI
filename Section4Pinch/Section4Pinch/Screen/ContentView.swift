5e9f0a6e24ceafd422a3946b7b078b8c030356e2
jewerlykim <jsjs21good@gmail.com>
2021-12-21 09:54:09 +0900

    section4 mid commit

---
+++ Section4Pinch/Section4Pinch/Screen/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  Section4Pinch
+//
+//  Created by 김보석 on 2021/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Property
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    // MARK: - Function
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
+    // MARK: - Content
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // MARK: - Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                // MARK: - 1 . Tap gesture
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    })
+                // MARK: - 2 . Drag gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                                
+                            }
+                    )
+                
+            } //: ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 2)) {
+                    isAnimating = true
+                }
+            })
+        } //: Navigation
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.light)
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
---
+++ Section4Pinch/Section4Pinch/Section4PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Section4PinchApp.swift
+//  Section4Pinch
+//
+//  Created by 김보석 on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Section4PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
